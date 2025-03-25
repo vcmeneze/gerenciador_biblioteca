3c2e465614b2f9235bdfc39ebd8068980e3c91cb
vcm7000 <vcm7000@gmail.com>
2025-03-24 21:40:22 -0300

    first commit

---
+++ src/Livro.java
@@ -0,0 +1,57 @@
+public class Livro {
+    private String nome;
+    private String autor;
+    private int ano;
+    private String isbn;
+    private final long id;
+    private static long proximoId = 1;  // Controla o próximo ID a ser atribuído
+
+    public Livro(String nome, String autor, int ano, String isbn) {
+        this.id = proximoId++;  // Atribui o próximo ID e incrementa para o próximo livro
+        this.nome = nome;
+        this.autor = autor;
+        this.ano = ano;
+        this.isbn = isbn;
+    }
+
+    public Livro() {
+        this("", "", 0, "");  // Chama o construtor principal com valores padrão
+    }
+
+    // Getters
+    public long getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Título: " + nome + ", Autor: " + autor +
+                ", Ano: " + ano + ", ISBN: " + isbn;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void menuPrincipal(){
+        System.out.println("Seja bem vindo ao gerenciador de biblioteca! O que deseja fazer hoje?");
+        System.out.println("1. Adicionar livro");
+        System.out.println("2. Listar livros");
+        System.out.println("3. Buscar livros por ISBN");
+        System.out.println("4. Remover livro");
+        System.out.println("5. Encerrar");
+    }
+
+    public static void gitmain(String[] args) {
+        ArrayList <Livro> biblioteca = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        // executar a aplicação enquanto a opção de encerrar nao for selecionada
+        do {
+            menuPrincipal();
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1: {
+                    System.out.println("Preencha as informações do livro que deseja adicionar:");
+                    System.out.println("Título:");
+                    String nome = scanner.nextLine();
+
+                    System.out.println("Autor:");
+                    String autor = scanner.nextLine();
+
+                    System.out.println("Ano de publicação:");
+                    int ano = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.println("ISBN do livro:");
+                    String isbn = scanner.nextLine();
+
+                    System.out.println("Informações inseridas pelo usuario:");
+                    System.out.println("Título: " + nome);
+                    System.out.println("Autor: " + autor);
+                    System.out.println("Ano de publicação: " + ano);
+                    System.out.println("ISBN: " + isbn);
+
+                    biblioteca.add(new Livro(nome, autor, ano, isbn));
+                    System.out.println("Livro adicionado à biblioteca");
+                    break;
+
+                }
+                case 2: {
+                    if (biblioteca.isEmpty()) {
+                        System.out.println("Não há nada para mostrar aqui.");
+                    } else {
+                        for (Livro livro : biblioteca) {
+                            System.out.println(livro);
+                        }
+                    }
+                    break;
+                }
+                case 3: {
+                    System.out.println("Digite o ISBN que deseja buscar:");
+                    String search = scanner.nextLine();
+                    biblioteca.stream()
+                            .filter(livro -> livro.getIsbn().equals(search))
+                            .forEach(System.out::println);
+                    break;
+                }
+                case 4: {
+                    System.out.println("Indique o número de ID do livro que deseja remover da lista: ");
+                    int toRemove = scanner.nextInt();
+                    scanner.nextLine();
+
+                    // o bool removido será true se existir um livro com a ID enviada pelo usuário
+                    boolean removido = biblioteca.removeIf(livro -> livro.getId() == toRemove);
+                    if (removido) {
+                        System.out.println("Livro removido!");
+                    } else {
+                        System.out.println("Não foi possível localilzar o livro com o id informado.");
+                    }
+                }
+            }
+        } while (opcao !=5); //encerra a aplicação
+    }
+}
